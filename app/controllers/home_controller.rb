e9cfdde088d106402047189d39c079b1ff7136d6
Student A Test <student+university-of-texas-rio-grande-valley1@codio.com>
2020-04-10 02:56:24 +0000

    add styling, redirects

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
     def index
-        @home = true
+        if user_signed_in?
+            redirect_to user_posts_path(current_user)
+        else
+            @home = true
+        end
     end
 end
