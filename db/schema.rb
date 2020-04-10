3142a7823e6195e0702305d430fb04b8e44999d6
Student A Test <student+university-of-texas-rio-grande-valley1@codio.com>
2020-04-10 02:12:28 +0000

    add posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_09_035417) do
+ActiveRecord::Schema.define(version: 2020_04_09_160927) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text "caption"
+    t.text "image_url"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
---
+++ spec/requests/post_request_spec.rb
@@ -0,0 +1,95 @@
+require 'rails_helper'
+
+RSpec.describe "Posts", type: :request do
+    fixtures :users, :posts
+      before do
+        @user = users(:tom)
+        @post = posts(:one)
+      end
+    
+     describe "When signed in" do
+       before do
+          sign_in @user
+       end
+         
+      it "should get index" do
+        get user_posts_url(@user)
+        assert_response :success
+      end
+
+      it "should get new" do
+        get new_user_post_url(@user)
+        assert_response :success
+      end
+
+      it "should create post" do
+        expect{
+            post user_posts_url(@user), params: { post: { caption: @post.caption, image_url: @post.image_url } }
+        }.to change(Post,:count).by 1
+        puts("USER: #{@user.id}")
+        puts("POST: #{Post.last.id}")
+        assert_redirected_to user_posts_url(@user)
+      end
+
+      it "should show post" do
+        get user_post_url(@user, @post)
+        assert_response :success
+      end
+
+      it "should get edit" do
+        get edit_user_post_url(@user,@post)
+        assert_response :success
+      end
+
+      it "should update post" do
+        patch user_post_url(@user, @post), params: { post: { caption: @post.caption, image_url: @post.image_url, } }
+        assert_redirected_to user_posts_url(@user)
+      end
+
+      it "should destroy post" do
+        expect{
+          delete user_post_url(@user, @post)
+        }.to change(Post,:count).by -1
+
+        assert_redirected_to user_posts_url(@user)
+      end
+    end
+
+    describe "when not signed in" do
+      it "should not get index" do
+        get user_posts_url(@user)
+        expect(response).to redirect_to(new_user_session_path)
+      end
+
+      it "should not get new" do
+        get new_user_post_url(@user)
+        expect(response).to redirect_to(new_user_session_path)
+      end
+
+      it "should not create post" do
+        post user_posts_url(@user), params: { post: { caption: @post.caption, image_url: @post.image_url } }
+        expect(response).to redirect_to(new_user_session_path)
+      end
+
+      it "should not show post" do
+        get user_post_url(@user, @post)
+        expect(response).to redirect_to(new_user_session_path)
+      end
+
+      it "should not get edit" do
+        get edit_user_post_url(@user, @post)
+        expect(response).to redirect_to(new_user_session_path)
+      end
+
+      it "should not update post" do
+        patch user_post_url(@user, @post), params: { post: { caption: @post.caption, image_url: @post.image_url } }
+        expect(response).to redirect_to(new_user_session_path)
+      end
+
+      it "should not destroy post" do
+        delete user_post_url(@user, @post)
+        expect(response).to redirect_to(new_user_session_path)
+      end
+
+    end
+end
