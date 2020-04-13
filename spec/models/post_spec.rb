7ba5a5c93612fdf3bfd72e3ac66c1a5a376ed22f
Eric Martinez <eric.michael.mtz@gmail.com>
2020-04-12 20:49:40 -0500

    add validations and tests for post model

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  it "requires an image_url to be valid" do
+    post = Post.new(image_url: nil, user_id: 1)
+    expect(post).not_to be_valid
+  end
+
+  it "requires a user_id to be valid" do
+    post = Post.new(image_url: "http://www.google.com/dog.jpeg", user_id: nil)
+    expect(post).not_to be_valid
+  end
+
+  it "is valid when given an image_url and user_id" do
+    post = Post.new(image_url: "http://www.google.com/dog.jpeg", user_id: 1)
+    expect(post).to be_valid
+  end
+end
