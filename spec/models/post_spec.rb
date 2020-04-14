e791a2ff59ef9b31281197167b012106327119ca
Eric Martinez <eric.michael.mtz@gmail.com>
2020-04-13 21:50:16 -0500

    fix some issues with turnip leftovers, and post spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,13 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+  before do
+    @user = User.create(email: "bob@bob.com", password: "password123")
+  end
+
   it "requires an image_url to be valid" do
-    post = Post.new(image_url: nil, user_id: 1)
+    post = Post.new(image_url: nil, user_id: @user.id)
     expect(post).not_to be_valid
   end
 
   it "requires a valid image_url to be valid" do
-    post = Post.new(image_url: "http://www.google.com", user_id: 1)
+    post = Post.new(image_url: "http://www.google.com", user_id: @user.id)
     expect(post).not_to be_valid
   end
 
@@ -17,7 +21,7 @@ RSpec.describe Post, type: :model do
   end
 
   it "is valid when given an image_url and user_id" do
-    post = Post.new(image_url: "http://www.google.com/dog.jpeg", user_id: 1)
+    post = Post.new(image_url: "http://www.google.com/dog.jpeg", user_id: @user.id)
     expect(post).to be_valid
   end
 end
